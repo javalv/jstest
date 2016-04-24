008ef06be7b1008db518c53f0a578adc8c4f4e04
lin <java.com.lv@163.com>
2016-04-24 16:45:23 +0800

    es6环境测试

---
+++ node/src/Person.js
@@ -0,0 +1,24 @@
+
+    //'use strict';
+    //class Person{
+    //    constructor(x,y){
+    //        this.x = x;
+    //        this.y = y;
+    //    }
+    //
+    //    toString() {
+    //        return '(' + this.x + ', ' + this.y + ')';
+    //    }
+    //}
+    'use strict';
+    //export function printf(x,y) {
+    //    //var p = new Person(x,y);
+    //    console.log(x + y);
+    //}
+
+    var firstName = 'Michael';
+    var lastName = 'Jackson';
+    var year = 1958;
+
+    export {firstName, lastName, year};
+
---
+++ node/src/exe.js
@@ -0,0 +1,4 @@
+'use strict';
+import * as printf from  './Person';
+
+//printf(100, 200);
